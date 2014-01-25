071c6070e7b6c7ac76643ec13ad70d0a30b6e823
ierceg <ivan@softwaremarbles.com>
2014-01-25 18:17:39 -0300

    Fixed all newly failing tests (1 failing, 1 pending as before) Made map view functions cleaner

--- test/init.js
+++ test/init.js
@@ -2,6 +2,9 @@ module.exports = require('should');
 
 var Schema = require('jugglingdb').Schema;
 
+//  Report pending tests when failing as in the past the adapter has had issues with infinite loops.
+require('mocha-unfunk-reporter').option('reportPending', true);
+
 global.getSchema = function() {
 
     var db = new Schema(__dirname + '/..', {
